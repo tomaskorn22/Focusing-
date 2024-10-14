7a2cf7ac50058e53796b38f2c01719edabf95f5e
Lucas Tjor <tjor75@gmail.com>
2024-10-14 09:34:25 -0300

    Creacion de model bd.cs

---
+++ Models/BD.cs
@@ -0,0 +1,9 @@
+using System.Data.SqlClient;
+using Dapper;
+
+public static class BD
+{
+    const string CONNECTION_STRING = "Server=localhost;DataBase=Focusing Empowering Minds;Trusted_Connection=True;";
+
+    
+}
