9e461475ac22b129034ec1735e23d02a832109a3
Lucas Tjor <tjor75@gmail.com>
2024-10-14 10:31:52 -0300

    Empezar a ver como hacemos el header y seguimos con las clases

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5d2db59e208869ca4917f9f7830668102276f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c911e08580eb882340ea79801504b285fbbf23c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
