21076e85a084217ffbe3a9c458a0ac9649653fa9
Lucas Tjor <tjor75@gmail.com>
2024-10-16 08:44:24 -0300

    Hacer page inicio sesion

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,4 +17,8 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult InicioSesion()
+    {
+        return View();
+    }
 }
--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29223c0f7706058b887a57f89395d38319584024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003f8b070a215b44421493f3d42c8bcef12a60a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
