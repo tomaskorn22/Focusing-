38adfe478efac8850c3c6fb6e4b8230eb13b1e64
TOMAS DAVID KLAWIR <48385206@ort>
2024-10-21 10:04:30 -0300

    Avances del index

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a5e481657999d6bb69e70df7d2c344474bdbaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae1380ed43a294e9c7c097a2b88dd1f5d31742f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
