a265c70f6a1883d6c51cfafba94650487109270f
TOMAS DAVID KLAWIR <48385206@ort>
2024-10-21 08:38:21 -0300

    Agregar el avatar al index

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21076e85a084217ffbe3a9c458a0ac9649653fa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a5e481657999d6bb69e70df7d2c344474bdbaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
