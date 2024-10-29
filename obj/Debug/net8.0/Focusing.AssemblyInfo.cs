a751d66dda687b31d3aeb400500950e8da91cfeb
Lucas Tjor <tjor75@gmail.com>
2024-10-29 08:55:11 -0300

    Avances multiples

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a07de1c5a9b78a7118434c7edb345de2959926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef41d8406aed79a0293c14292f5428f89a22f3ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
