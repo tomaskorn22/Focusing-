5e84e464d499777707b2e5af7296ab886e7404a3
TOMAS DAVID KLAWIR <48385206@ort>
2024-10-30 07:49:59 -0300

    Clase BD empezada con agregar usuario hecho

--- Models/BD.cs
+++ Models/BD.cs
@@ -5,5 +5,10 @@ public static class BD
 {
     const string CONNECTION_STRING = "Server=localhost;DataBase=Focusing Empowering Minds;Trusted_Connection=True;";
 
-    
+    public static void AgregarUsuario(Usuario Usu){
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "INSERT INTO Usuarios (Nombre, Apellido, Mail, Contraseña, Edad) VALUES (@pNombre, @pApellido, @pMail, @pConraseña, @pEdad)";
+            db.Execute(sql, new{pNombre = Usu.Nombre, pApellido = Usu.Apellido,  pMail = Usu.Mail, pContraseña = Usu.Contraseña, pEdad = Usu.Edad});
+        }
+    }
 }
