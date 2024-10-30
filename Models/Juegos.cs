3ea8d3c47ce872a11be524373723c7d50f3259de
Lucas Tjor <tjor75@gmail.com>
2024-10-30 08:35:34 -0300

    Cambios en model juegos

--- Models/Juegos.cs
+++ Models/Juegos.cs
@@ -1,6 +1,8 @@
 public class Juegos
 {
     public string titulo { get; set; }
-    public DateTime fechaCreacion  { get; set; }
+    public string descripcion  { get; set; }
+    public string? foto {get;set;}
+
 
 }
