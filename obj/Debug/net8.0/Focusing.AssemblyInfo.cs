a15b2d716b6c2309b88c5394c8f1c35bfcebdca6
Lucas Tjor <tjor75@gmail.com>
2024-10-31 11:44:39 -0300

    Seguimos avanzando con la coneccion de la base de datos

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a08372e0d01776d51dacd7edfed2c851d7c9d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295a78b11afcd96429176f55e81daabda2f52243")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
