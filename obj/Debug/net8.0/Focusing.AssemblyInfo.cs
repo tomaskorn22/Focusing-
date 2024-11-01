fbe52fe25eea2db012e60653ef47ab08d28223d1
Lucas Tjor <tjor75@gmail.com>
2024-11-01 13:42:57 -0300

    Ajustes en el diseño del crearcuenta.cshtml

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8faba2840e6da51c937304bc5b05dc2bd47b1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b6d0775895fefa6e6da80c3e8c2db83b9f921a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
