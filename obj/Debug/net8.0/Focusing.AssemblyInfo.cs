09c2c0b9acd6847c4b72d1e1d178cf75e16c8957
Lucas Tjor <tjor75@gmail.com>
2024-11-14 10:35:41 -0300

    Cambios

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf0f280778bef5fec3302e3a9d921685e836a3fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc07b6bbad2ac4ade197d15801f587baa7a4c1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
