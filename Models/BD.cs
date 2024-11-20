1bcb12900fb71a7e04921da3434278bed075db94
Lucas Tjor <tjor75@gmail.com>
2024-11-20 14:14:31 -0300

    1 era solucion

--- Models/BD.cs
+++ Models/BD.cs
@@ -132,8 +132,7 @@ public static class BD
 {
     using (SqlConnection db = new SqlConnection(_connectionString))
     {
-        string sql = "INSERT INTO SentimientosXUsuarios (Id_usuario, Id_sentimiento) VALUES (@pId_usuario, @pId_sentimiento)";
-        
+        string sql = "INSERT INTO Sentimientos (Id_usuario, Id_sentimiento) VALUES (@pId_usuario, @pId_sentimiento)";
         db.Execute(sql, new { pId_usuario = Id_usuario, pId_sentimiento = Id_sentimiento });
     }
 }
--- obj/Debug/net7.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net7.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b05c1f38cd7974f310582502008bd6e561f65d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6596a6da4fe38eb4822bc26bc746caab2142ee27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
