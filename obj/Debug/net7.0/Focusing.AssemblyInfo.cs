6596a6da4fe38eb4822bc26bc746caab2142ee27
Lucas Tjor <tjor75@gmail.com>
2024-11-20 09:05:12 -0300

    Aegumos

--- obj/Debug/net7.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net7.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06916936a4ae296658c26013c51a03d22eeefb4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b05c1f38cd7974f310582502008bd6e561f65d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
