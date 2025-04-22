5295bcaac93c00f820fcc29f930e2d823fedf53a
TOMAS DAVID KLAWIR <48385206@ort>
2025-04-22 11:16:21 -0300

    Index y tips mejorados

--- obj/Debug/net8.0/Focusing.AssemblyInfo.cs
+++ obj/Debug/net8.0/Focusing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e898169f8abab31cdc943d246a534d36d5b2b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f10d0a6c3756d848efa419c3356ae5f85a85b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Focusing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
